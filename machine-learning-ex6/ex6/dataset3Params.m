ac5b2428222e2aa04373a3acd54e1ff6b3fdc88f
Alex Chen <n.a.chen@accenture.com>
2019-10-21 16:57:18 +1100

    ex6 full answers

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,6 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+
+% possible_vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% test_result = zeros(9, 9);
+
+% test_result(1, 2:end) = possible_vals;
+% test_result(2:end, 1) = possible_vals';
+
+
+% for i=1:length(possible_vals)
+%     for j=1:length(possible_vals)
+%         fprintf('testing C:%f , sigma: %f', possible_vals(i), possible_vals(j));
+%         model= svmTrain(X, y, possible_vals(i), @(x1, x2) gaussianKernel(x1, x2, possible_vals(j)));
+%         predictions = svmPredict(model, Xval);
+
+
+%         test_result(i+1, j+1) = mean(double(predictions ~= yval))
+%     end
+% end
+
 
 
 
--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -15,8 +15,8 @@ sim = 0;
 %               sigma
 %
 %
-
-
+distance = sum((x1 - x2).^2);
+sim = exp(-1* distance / (2 * sigma ^ 2));
 
 
 
